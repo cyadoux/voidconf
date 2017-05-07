6d77c3651f540207ca51b496b3bf8dc11c1c89e6
Nathan Chapman <nathanchapman@protonmail.com>
2017-05-07 08:11:13 -0700

    st: fix space-tabs into tabs

--- st/config.def.h
+++ st/config.def.h
@@ -85,29 +85,31 @@ static unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
-  /* 8 normal colors */
-  [0] = "#282a2e", /* black   */
-  [1] = "#a54242", /* red     */
-  [2] = "#8c9440", /* green   */
-  [3] = "#de935f", /* yellow  */
-  [4] = "#5f819d", /* blue    */
-  [5] = "#85678f", /* magenta */
-  [6] = "#5e8d87", /* cyan    */
-  [7] = "#707880", /* white   */
+	/* 8 normal colors */
+	"#282a2e", /* black   */
+	"#a54242", /* red     */
+	"#8c9440", /* green   */
+	"#de935f", /* yellow  */
+	"#5f819d", /* blue    */
+	"#85678f", /* magenta */
+	"#5e8d87", /* cyan    */
+	"#707880", /* white   */
 
-  /* 8 bright colors */
-  [8]  = "#373b41", /* black   */
-  [9]  = "#cc6666", /* red     */
-  [10] = "#b5bd68", /* green   */
-  [11] = "#f0c674", /* yellow  */
-  [12] = "#81a2be", /* blue    */
-  [13] = "#b294bb", /* magenta */
-  [14] = "#8abeb7", /* cyan    */
-  [15] = "#c5c8c6", /* white   */
+	/* 8 bright colors */
+	"#373b41", /* black   */
+	"#cc6666", /* red     */
+	"#b5bd68", /* green   */
+	"#f0c674", /* yellow  */
+	"#81a2be", /* blue    */
+	"#b294bb", /* magenta */
+	"#8abeb7", /* cyan    */
+	"#c5c8c6", /* white   */
 
-  /* special colors */
-  [256] = "#1d1f21", /* background */
-  [257] = "#c5c8c6", /* foreground */
+	[255] = 0,
+
+	/* special colors */
+	"#1d1f21", /* background */
+	"#c5c8c6", /* foreground */
 };
 
 /*
